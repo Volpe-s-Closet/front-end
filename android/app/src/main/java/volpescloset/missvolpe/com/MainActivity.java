1011fdec121e31abd9b6aec4aca5ec480dc8514a
TheProcedural <contact@theprocedural.com>
2025-06-22 19:02:13 +0100

    Add capacitor

---
+++ android/app/src/main/java/volpescloset/missvolpe/com/MainActivity.java
@@ -0,0 +1,5 @@
+package volpescloset.missvolpe.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'volpescloset.missvolpe.com',
+  appName: "Volpe's Closet",
+  webDir: '.output/public'
+};
+
+export default config;
